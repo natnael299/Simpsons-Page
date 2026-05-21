584781bb5010a37078d685cd6a1ed9dc8c263de5
natnael <natibeyene49@gmail.com>
2026-05-21 12:58:12 +0300

    create a gallery page

--- gallery/gallary.js
+++ gallery/gallary.js
@@ -1,9 +1,10 @@
-import { locations } from "./scripts/utils.js";
+import { locations } from "../scripts/utils.js";
 
 const urlParam = new URLSearchParams(document.location.search);
 const id = urlParam.get("id");
 
-const selectedLocation = locations.find((location) => location.id == id) ?? locations[0];
+const selectedLocation =
+  locations.find((location) => location.id == id) ?? locations[0];
 const container = document.querySelector(".container");
 
 document.title = `${selectedLocation.name} | Gallery`;
@@ -15,23 +16,25 @@ const galleryMarkup = `
 				<img src="${selectedLocation.coverImage}" alt="${selectedLocation.name}" />
 			</div>
 			<div class="galleryHeroCopy">
-				<p class="galleryEyebrow">${selectedLocation.shortDescription}</p>
+				<p class="galleryEyebrow"><i class='fas fa-location-dot'></i> ${selectedLocation.shortDescription}</p>
 				<h1>${selectedLocation.name}</h1>
 				<p>${selectedLocation.fullDescription}</p>
+				<a href='./springfield.html'>&leftarrow;  Back to Springfield</a>
 			</div>
 		</section>
 
+		<h1 class='galleryHeader'> ${selectedLocation.name} Gallery</h1>
 		<section class="galleryGrid" aria-label="${selectedLocation.name} image gallery">
 			${selectedLocation.gallery
-				.map(
-					(item) => `
+        .map(
+          (item) => `
 						<figure class="galleryCard">
 							<img src="${item.image}" alt="${item.alt}" ${item.fullscreen ? 'data-fullscreen="true"' : ""} />
 							<figcaption>${item.title}</figcaption>
 						</figure>
 					`,
-				)
-				.join("")}
+        )
+        .join("")}
 		</section>
 	</main>
 `;
